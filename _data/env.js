d41e5e69be0a7245dcbdcc2150986fac68f79e43
peteraiello <hello@peteraiello.dev>
2026-02-13 17:35:36 -0500

    Vercel env

--- _data/env.js
+++ _data/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	environment: process.env.MY_ENVIRONMENT,
+	environment: process.env.VERCEL_ENV || process.env.MY_ENVIRONMENT || "development",
     localhost: "http://localhost:8080/",
     development: "https://j-akhtar-11vnty.vercel.app/",
     production: "https://www.jamilayakhtar.com/"
